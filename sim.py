3e437ab6171e4f7c96b2914b2eacb19ca98623bc
LBartolini <l.bartolini02@gmail.com>
2020-08-27 10:13:38 +0200

    Probability works fine

--- sim.py
+++ sim.py
@@ -5,10 +5,29 @@ from graphics import *
 width, height = 600, 600
 MAX_GK_DIM = 80
 
-center_goal = (0, (-height/2)+20)
+center_goal = (0, int(-height/2)+20)
 goal_dim = int(width*0.25)
 gk_dim = min(int(goal_dim*0.5), MAX_GK_DIM)
 penalty_area_r = int(width*0.25)
+
+# https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines
+
+def line(p1, p2):
+    A = (p1[1] - p2[1])
+    B = (p2[0] - p1[0])
+    C = (p1[0]*p2[1] - p2[0]*p1[1])
+    return A, B, -C
+
+def intersection(L1, L2):
+    D  = L1[0] * L2[1] - L1[1] * L2[0]
+    Dx = L1[2] * L2[1] - L1[1] * L2[2]
+    Dy = L1[0] * L2[2] - L1[2] * L2[0]
+    if D != 0:
+        x = Dx / D
+        y = Dy / D
+        return x,y
+    else:
+        return False
 
 def distance(p1, p2):
     return np.sqrt(((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2))
@@ -24,6 +43,70 @@ def get_inputs(atk, center_goal, goal_dim):
 
     return dist/100, np.degrees(angle)/100
 
+def check_gk_pos(atk_left_line, atk_right_line, vertical_gk, gk):
+    int_left_ver = intersection(atk_left_line, vertical_gk)
+    int_right_ver = intersection(atk_right_line, vertical_gk)
+
+    if not int_left_ver and not int_right_ver:
+        if int_left_ver[1] <= gk[1] <= int_right_ver[1] or int_right_ver[1] <= gk[1] <= int_left_ver[1]:
+            return True
+        else:
+            return False
+    else:
+        return True
+    
+
+def get_prob(center_goal, atk, gk, gk_dim, goal_dim):
+    prob = 1
+    #gk = (r*np.cos(angle), r*np.sin(angle)+center_goal[1]) # convert polar to cartesian coordinates
+    left_goal = center_goal[0]-(goal_dim/2), center_goal[1]
+    right_goal = center_goal[0]+(goal_dim/2), center_goal[1]
+
+    atk_gk_line = line(atk, gk)
+    atk_left_line = line(atk, left_goal)
+    atk_right_line = line(atk, right_goal)
+    vertical_gk = line(gk, (gk[0],gk[1]+1))
+
+    if check_gk_pos(atk_left_line, atk_right_line, vertical_gk, gk):
+        try:
+            slope_atk_gk_line = atk_gk_line[0]/atk_gk_line[1] # slope = -a/b
+        except ZeroDivisionError:
+            slope_atk_gk_line = 10e6
+
+        slope_perp = -1 / slope_atk_gk_line # m
+        try:
+            int_perp = gk[1] - (-slope_perp * gk[0]) # q
+        except ZeroDivisionError:
+            if gk[0] == 0:
+                int_perp = gk[1]
+
+        perp_line = (slope_perp, 1, int_perp)
+        left_int = intersection(atk_left_line, perp_line)
+        right_int = intersection(atk_right_line, perp_line)
+
+        #prob_left = (distance(left_int, gk) / (distance(left_int, gk) - gk_dim/2)) - 1
+        #prob_right = (distance(right_int, gk) / (distance(right_int, gk) - gk_dim/2)) - 1
+        prob_left = (max(distance(left_int, gk) - gk_dim/2, 0) / distance(left_int, gk))
+        prob_right = (max(distance(right_int, gk) - gk_dim/2, 0) / distance(right_int, gk))
+
+        prob = prob_left + prob_right
+
+    print(f"ATK: {atk}")
+    print(f"GK: {gk}")
+    print(f"LEFT: {left_goal}")
+    print(f"RIGHT: {right_goal}")
+    print(f"GK_DIM: {gk_dim}")
+    print(f"LEFT_INT: {left_int}")
+    print(f"RIGHT_INT: {right_int}")
+    print(f"ATK LEFT: {atk_left_line}")
+    print(f"ATK RIGHT: {atk_right_line}")
+    print(f"PERP: {perp_line}")
+    print(f"PROB LEFT: {prob_left}")
+    print(f"PROB RIGHT: {prob_right}")
+
+
+    return prob
+
 def simulation(net, rounds=100):
     score = 0
 
@@ -37,16 +120,20 @@ def simulation(net, rounds=100):
         # ask the network where to position
         inp = np.array(get_inputs(atk, center_goal, goal_dim))
         pred = net.forward_propagation(inp)
-        pred[0] *= penalty_area_r # distance to move
-        pred[1] = np.degrees(pred[1]*2*np.pi) # direction where to move from the center of the goal
+        pred[0] *= penalty_area_r # distance to move (aka r)
+        pred[1] = np.degrees(pred[1]*2*np.pi) # direction where to move from the center of the goal (aka angle)
 
-    # select where to shoot
-    # store if GK took goal or not
+        # select where to shoot
+        gk_pos = (pred[0]*np.cos(pred[1]), pred[0]*np.sin(pred[1])+center_goal[1]) # convert polar to cartesian coordinates
+        prob_goal = get_prob(center_goal, atk, gk_pos, gk_dim, goal_dim)
+
+        # store if GK took goal or not
     
     return score
 
 if __name__ == '__main__':
     #setup(width, height, goal_dim, penalty_area_r)
+    '''
     net = Network(np.concatenate(([2], [10], [2])))        
 
     inp = np.array(get_inputs((-10, 10), center_goal, goal_dim))
@@ -55,3 +142,7 @@ if __name__ == '__main__':
     pred[1] = np.degrees(pred[1]*2*np.pi)
 
     print(pred)
+    '''
+    atk = (-200, -100)
+    gk_ = (-100, -200)
+    print(get_prob(center_goal, atk, gk_, gk_dim, goal_dim))
