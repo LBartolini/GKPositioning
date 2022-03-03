73f10723d941044f6a8f3c65935880a9a2deccdd
LBartolini <l.bartolini02@gmail.com>
2022-03-03 11:34:57 +0100

    seems to learn; need to check gk position on sim

--- main.py
+++ main.py
@@ -1,43 +1,72 @@
 import numpy as np
+from typing import List, Tuple
 from sim import *
 from net import *
 
 EPOCHS_SIM = 10
-POPULATION_SIM=100
+POPULATION_SIM=50
 
-EPOCH_TRAINING = 3000
+EPOCH_TRAINING = 100
 LR = 0.1
 
-def get_train_set(X, y, scores, top=0.3):
+def one_hot(value, dimension) -> List:
+    x = np.zeros(dimension+1, dtype=np.int8)
+    x[int(value+((dimension+1)/2))] = 1
+
+    return list(x)
+
+def get_train_set(X, y, scores, env, top=0.3):
     treshold = sorted(scores)[-int(len(scores)*top)]
 
-    X_train, y_train = [], []
+    X_train, yx_train, yy_train = [], [], []
 
     for i in range(len(scores)):
         if scores[i] >= treshold:
-            X_train.append([X[i]])
-            y_train.append([y[i]])
+            x_one_hot, y_one_hot = get_one_hot_from_point(Point(X[i][0], X[i][1]), env)
+            X_train.append([x_one_hot+y_one_hot])
 
-    return np.array(X_train), np.array(y_train), treshold
+            x_one_hot, y_one_hot = get_one_hot_from_point(Point(y[i][0], y[i][1]), env)
+            yx_train.append([x_one_hot])
+            yy_train.append([y_one_hot])
+
+    return np.array(X_train), np.array(yx_train), np.array(yy_train), np.array(treshold)
+
+def get_one_hot_from_point(point: Point, env: Env) -> Tuple[List, List]:
+    x_one_hot = one_hot(point.x, env.width)
+    y_one_hot = one_hot(point.y, env.height)
+
+    return x_one_hot, y_one_hot
+
+def create_net(inp_dim, out_dim, n_hidden=5) -> Network:
+    net = Network()
+    net.add(FCLayer(inp_dim, 100))
+    net.add(ActivationLayer(tanh, tanh_prime))
+    
+    for _ in range(n_hidden):
+        net.add(FCLayer(100, 100))
+        net.add(ActivationLayer(tanh, tanh_prime))
+
+    net.add(FCLayer(100, out_dim))
+    net.add(ActivationLayer(sigmoid, sigmoid_prime))
+
+    net.use(mse, mse_prime)
+
+    return net
+
+def get_gk_position(atk, net_x, net_y, env) -> Point:
+    x_one_hot, y_one_hot = get_one_hot_from_point(atk, env)
+    inp = np.array([x_one_hot+y_one_hot])
+
+    pred_x = net_x.predict(inp)[0][0]
+    pred_y = net_y.predict(inp)[0][0]
+
+    return Point(np.argmax(pred_x)-env.width/2, np.argmax(pred_y)-env.height/2)
 
 def main():
     env = Env()
 
-    net = Network()
-    net.add(FCLayer(2, 100))
-    net.add(ActivationLayer(tanh, tanh_prime))
-    net.add(FCLayer(100, 100))
-    net.add(ActivationLayer(tanh, tanh_prime))
-    net.add(FCLayer(100, 100))
-    net.add(ActivationLayer(tanh, tanh_prime))
-    net.add(FCLayer(100, 100))
-    net.add(ActivationLayer(tanh, tanh_prime))
-    net.add(FCLayer(100, 100))
-    net.add(ActivationLayer(tanh, tanh_prime))
-    net.add(FCLayer(100, 2))
-    net.add(ActivationLayer(sigmoid, sigmoid_prime))
-
-    net.use(mse, mse_prime)
+    net_x = create_net(env.width+env.height+2, env.width+1)
+    net_y = create_net(env.width+env.height+2, env.height+1)
 
     for k in range(EPOCHS_SIM):
         print(f"-----\nMAIN EPOCH: {k}\n")
@@ -46,16 +75,20 @@ def main():
         scores = [] # scores recorder for each iteration
 
         for i in range(POPULATION_SIM):
-            atk, gk, score = env.sim(net)
+            atk = env.get_random_atk_position()
+            gk = get_gk_position(atk, net_x, net_y, env)
+
+            score = env.sim(atk, gk)
 
             X.append(atk.get_tuple())
             y.append(gk.get_tuple())
             scores.append(score)
         
-        X_train, y_train, treshold = get_train_set(X, y, scores)
+        X_train, yx_train, yy_train, treshold = get_train_set(X, y, scores, env)
         print(f"Treshold: {treshold}\n\n")
 
-        net.fit(X_train, y_train, epochs=EPOCH_TRAINING, learning_rate=LR)
+        net_x.fit(X_train, yx_train, epochs=EPOCH_TRAINING, learning_rate=LR)
+        net_y.fit(X_train, yy_train, epochs=EPOCH_TRAINING, learning_rate=LR)
 
 
 if __name__ == '__main__':
--- sim.py
+++ sim.py
@@ -68,18 +68,6 @@ class Env:
         b=3/2*self.gk_dim
         return coeff/(a*np.sqrt(2*np.pi))*(np.exp(1)**(-0.5*(((x-b)/a)**2)))
 
-    def atk_polar(self, atk: Point):
-        # get polar coordinates of atk
-        dist = Point.distance(atk, self.center_goal)
-
-        if atk.x != self.center_goal.x:
-            angle_atk_goal = (atk.y-self.center_goal.y)/(atk.x-self.center_goal.x)
-            angle = np.degrees(np.arctan(angle_atk_goal))
-        else:
-            angle = 90
-
-        return dist/100, angle/100
-
     def compute_perp_atk_gk(self, atk_gk_line: Line, gk: Point) -> Line:
         try:
             slope_atk_gk_line = atk_gk_line.a/atk_gk_line.b  # slope = -a/b
@@ -97,7 +85,7 @@ class Env:
         return Line(slope_perp, 1, int_perp)
     
 
-    def get_prob(self, atk: Point, gk: Point, lob=False):
+    def get_prob(self, atk: Point, gk: Point, lob=True):
         probs = []
 
         #gk = (r*np.cos(angle), r*np.sin(angle)+center_goal[1]) # convert polar to cartesian coordinates
@@ -134,34 +122,22 @@ class Env:
 
             probs.append((prob_lob, 0.2))
 
-
         #calculating final prob
         prob = min(sum([x[0]*x[1] for x in probs]), 1)
 
         return prob
+    
+    def get_random_atk_position(self):
+        x = np.random.randint(-self.width/2, self.width/2)
+        y = min(np.random.randint(-self.height/2, self.height/2)+self.GOAL_LINE, self.height/2)
 
-    def get_gk_position(self, atk: Point, net) -> Point:
-        inp = np.array([self.atk_polar(atk)])
-        pred = net.predict(inp)[0][0]
-        pred[0] *= self.penalty_area_r # distance to move (aka r)
-        pred[1] = np.degrees(pred[1]*np.pi) # direction where to move from the center of the goal (aka angle)
-        
-        # convert polar to cartesian coordinates
-        gk_pos = Point(pred[0]*np.cos(pred[1]), self.center_goal.y+pred[0]*np.sin(pred[1])) 
+        return Point(x, y) 
 
-        return gk_pos
-
-    def sim(self, net, steps=50):
+    def sim(self, atk, gk_pos, steps=50):
         #returns the score obtained by the agent on a given atk position
         score = 0
 
-        # random select spot for attacker
-        x = np.random.randint(-self.width/2, self.width/2)
-        y = min(np.random.randint(-self.height/2, self.height/2)+self.GOAL_LINE, self.height/2)
-        atk = Point(x, y)
-
-        # ask the network where to position the goalkeeper
-        gk_pos = self.get_gk_position(atk, net)
+        # TODO check gk position
 
         # compute probability of scoring
         prob_goal = self.get_prob(atk, gk_pos)*1000 # 1_000 for accuracy
@@ -172,7 +148,7 @@ class Env:
             if np.random.randint(0, 1000) > prob_goal:
                 score += 1
                
-        return atk, gk_pos, score
+        return score
 
 if __name__ == '__main__':
     env = Env()
